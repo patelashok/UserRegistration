2bffaebdb4230a598107e44e6d427ea6392f55a5
patelashok <ashok.patel2917@gmail.com>
2021-06-05 18:35:04 +0530

    Using Lambda Function

---
+++ UserRegistrationException.java
@@ -0,0 +1,13 @@
+package userregistration;
+
+public class UserRegistrationException extends Exception{
+    /**
+     * this method creates exception for invalid outcomes.
+     * @return error message as invalid output.
+     */
+    String errorMessage;
+    public UserRegistrationException(String message) {
+        this.errorMessage=message;
+
+    }
+}
